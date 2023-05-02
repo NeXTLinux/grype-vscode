1af0b896cd56de21eafbbff82e34ffea3bea81ee
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-05-02 00:59:16 -0700

    Create extension.ts
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ src/extension.ts
@@ -0,0 +1,23 @@
+import * as vscode from "vscode";
+import { config } from "./config";
+import { determinePlatform } from "./Platform";
+import GrypeExtension from "./GrypeExtension";
+
+export async function activate(
+  context: vscode.ExtensionContext
+): Promise<void> {
+  const extension = new GrypeExtension(context, config, determinePlatform());
+
+  try {
+    await extension.activate();
+    console.log("grype extension activated");
+  } catch (e) {
+    console.error(e);
+    console.error("unsuccessful grype extension activation");
+  }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(): void {
+  // implement
+}
